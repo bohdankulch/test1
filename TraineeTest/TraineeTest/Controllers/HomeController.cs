4ae23f129177d63caef17b42ecb4a651273b9ece
Bogdan Kulchytskyi <bogdankulch244@gmail.com>
2021-04-26 22:25:32 +0300

    no message

---
+++ TraineeTest/TraineeTest/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TraineeTest.Models;
+
+namespace TraineeTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.ContactManagers.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ContactManager contactManager)
+        {
+            db.ContactManagers.Add(contactManager);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+            if (ModelState.IsValid)
+                return Content($"{contactManager.Name} - {contactManager.Phone}");
+            else
+                return View(contactManager);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                ContactManager user = await db.ContactManagers.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                ContactManager user = await db.ContactManagers.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ContactManager contactManager)
+        {
+            db.ContactManagers.Update(contactManager);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                ContactManager user = await db.ContactManagers.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                ContactManager user = new ContactManager { Id = id.Value };
+                db.Entry(user).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ TraineeTest/TraineeTest/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraineeTest.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<ContactManager> ContactManagers { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   
+        }
+    }
+}
